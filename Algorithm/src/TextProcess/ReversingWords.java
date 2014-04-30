2f664257b466fc8595b7f1567dfab97495f38bfe
Tom Wu <tom.wu@nurun.com>
2014-04-29 20:39:24 -0400

    - ReversingWords

--- Algorithm/src/TextProcess/ReversingWords.java
+++ Algorithm/src/TextProcess/ReversingWords.java
@@ -1,5 +1,7 @@
 package TextProcess;
 
+import java.util.StringTokenizer;
+
 public class ReversingWords {
 
 	/**
@@ -7,8 +9,35 @@ public class ReversingWords {
 	 */
 	public static void main(String[] args) {
 		String input = "Write a function to reverse the order of words in a string in place";
-		
-		
+
+        // Step 1 reverse first to last char
+        String newInput = reverseChar(input);
+        System.out.println(newInput);
+
+        // Step 2 reverse char in each word delimited by space
+        StringTokenizer st = new StringTokenizer(newInput, " ");
+        String finalInput = "";
+        while (st.hasMoreTokens()) {
+            if (finalInput.equals(null)) {
+                finalInput = reverseChar(st.nextToken());
+            } else {
+                finalInput = finalInput + " " + reverseChar(st.nextToken());
+            }
+        }
+        System.out.println(finalInput);
+
 	}
+
+    public static String reverseChar(String input) {
+        StringBuilder newInput = new StringBuilder(input);
+        int i = 0;  // beginning
+        int j = input.length() - 1; // end
+        for (i = 0; i < input.length(); i++) {
+            newInput.setCharAt(i, input.charAt(j));
+            j--;
+        }
+
+        return newInput.toString();
+    }
 
 }
